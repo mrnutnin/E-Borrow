f4ea88e6a8ddf237cb3f39f5b06ee1ad6c0898c7
MrNut <porinut1@gmail.com>
2020-08-19 10:12:20 +0700

    secound commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.auth.app')
 
 @section('content')
 <div class="container">
@@ -20,4 +20,25 @@
         </div>
     </div>
 </div>
+@endsection --}}
+
+@extends('layouts.home.app')
+
+@section('title', 'Main page')
+
+@section('content')
+    <div class="wrapper wrapper-content animated fadeInRight">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center m-t-lg">
+                            <h1>
+                                Welcome in INSPINIA Laravel Starter Project
+                            </h1>
+                            <small>
+                                It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects.
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </div>
 @endsection
