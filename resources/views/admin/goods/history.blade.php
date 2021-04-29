8e30ffb3c2e42f3506aca0e7ade8a05f6e9f3e81
MrNut <porinut1@gmail.com>
2021-04-29 19:06:35 +0700

    make it better

--- resources/views/admin/goods/history.blade.php
+++ resources/views/admin/goods/history.blade.php
@@ -103,7 +103,7 @@ $("#history_list_table").ready(function () {
     'columnDefs': [
         {
             "targets": [0, 1, 2, 3, 4, 5,6,7,8],
-            "className": "text-center",
+            "className": "",
         },
     ],
     "columns": [{
